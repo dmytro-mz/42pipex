acda94f1c8fa5b36a5a3ab2c7bbee4c217f86935
Dmytro Moroz <dmoroz@c4r1s4.42warsaw.pl>
2024-04-24 19:47:29 +0200

    fix bugs

--- pipex.c
+++ pipex.c
@@ -18,11 +18,10 @@ int	main(int ac, char *av[], char **envp)
 {
 	t_pipex_state	state;
 	int				i;
-	pid_t			pid;
 
 	setup(ac, av, &state);
 	main_loop(av, envp, &state);
-	clean_state(&state, i - 1);
+	clean_state(&state, state.n_cmd - 1);
 	i = 0;
 	while (i < state.n_cmd)
 	{
@@ -44,13 +43,13 @@ void	main_loop(char **av, char **envp, t_pipex_state *state)
 	{
 		if (i < state->n_cmd - 1)
 			if (pipe(&(state->pipes[i * 2])) == -1)
-				clean_n_exit(&state, i - 1, &exit_with_perror, "pipe");
+				clean_n_exit(state, i - 1, &exit_with_perror, "pipe");
 		pid = fork();
 		if (pid == -1)
-			clean_n_exit(&state, i, &exit_with_perror, "fork");
+			clean_n_exit(state, i, &exit_with_perror, "fork");
 		if (pid == 0)
 		{
-			run_cmd(&state, av[2 + i + state->here_doc], i, envp);
+			run_cmd(state, av[2 + i + state->here_doc], i, envp);
 		}
 		i++;
 	}
